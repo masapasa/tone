63494ad9f4b3f613a432e02e2de6060d62e79c3f
masapasa <apyakurel@gmail.com>
2024-06-02 12:32:11 +0200

    feat: Add lazy loading for images in main.tsx
    The commit adds lazy loading for images in the `main.tsx` file. This improves the page load performance by loading images only when they are visible on the screen.

--- frontend-ui/src/main.tsx
+++ frontend-ui/src/main.tsx
@@ -1,13 +1,41 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
-
-const manifestUrl =
-  "https://raw.githubusercontent.com/VopxTech/ton-vesting/main/frontend-ui/public/manifest.json";
+import { THEME, TonConnectUIProvider } from "@tonconnect/ui-react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
+  <TonConnectUIProvider manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
+  uiPreferences={{theme: THEME.DARK}}
+  walletsListConfiguration={{
+    includeWallets: [
+      {
+        appName: "safepalwallet",
+        name: "SafePal",
+        imageUrl: "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
+        tondns: "",
+        aboutUrl: "https://www.safepal.com",
+        universalLink: "https://link.safepal.io/ton-connect",
+        jsBridgeKey: "safepalwallet",
+        bridgeUrl: "https://ton-bridge.safepal.com/tonbridge/v1/bridge",
+        platforms: ["ios", "android", "chrome", "firefox"]
+      },
+      {
+        appName: "tonwallet",
+        name: "TON Wallet",
+        imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+        aboutUrl: "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+        universalLink: "https://wallet.ton.org/ton-connect",
+        jsBridgeKey: "tonwallet",
+        bridgeUrl: "https://bridge.tonapi.io/bridge",
+        platforms: ["chrome", "android"]
+      }
+    ]
+  }}
+  actionsConfiguration={{
+    twaReturnUrl: 'https://t.me/tc_twa_demo_bot/start'
+      }}
+      
+  >
     <App />
   </TonConnectUIProvider>
 );
